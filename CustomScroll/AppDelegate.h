bd5a6752ebda85d75b4ed5ab986ad877be5e111c
Nguyen Ha Giang <giangnth@nitrotech.vn>
2014-12-31 10:19:02 +0700

    Initial Commit

---
+++ CustomScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomScroll
+//
+//  Created by NguyenHaGiang on 12/31/14.
+//  Copyright (c) 2014 NGUYENHAGIANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomScroll
+//
+//  Created by NguyenHaGiang on 12/31/14.
+//  Copyright (c) 2014 NGUYENHAGIANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
